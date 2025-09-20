dde7cf299be95b7f5bf7f5d54e2f41eb2bbedeea
clr <clrcera05@gmail.com>
2025-09-20 15:25:42 -0300

    mt foda

--- main.py
+++ main.py
@@ -1,11 +1,68 @@
+import os
 from video import VideoMaker
+import heapq
+import cv2
+import numpy as np
 
 
 def main():
-    video_maker = VideoMaker("MyVideo", "100712_sat.jpg")
-    for i in range(150, 200):
-        for j in range(150, 200):
-            video_maker.change_pixel(i, j, (0, 255, 0))  # Change pixels to green
+    astar((94, 182), (322, 630), "100712")
+
+
+def distance(a, b):
+    return ((a[0] - b[0]) ** 2 + (a[1] - b[1]) ** 2) ** 0.5
+
+
+def bfs(origin, target, image_path):
+    video_maker = VideoMaker("BFS_Visualization", image_path)
+    queue = [origin]
+    visited = set()
+    directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]  # Up, Down, Left, Right
+    while queue:
+        current = queue.pop(0)
+        if current == target:
+            break
+        for direction in directions:
+            neighbor = (current[0] + direction[0], current[1] + direction[1])
+            if (
+                0 <= neighbor[0] < video_maker.frame.shape[1]
+                and 0 <= neighbor[1] < video_maker.frame.shape[0]
+                and neighbor not in visited
+            ):
+                visited.add(neighbor)
+                queue.append(neighbor)
+                video_maker.change_pixel(
+                    neighbor[0], neighbor[1], (255, 0, 0)
+                )  # Mark as visited
+    video_maker.release()
+
+
+def astar(origin, target, image_num):
+    video_maker = VideoMaker("AStar_Visualization", image_num + "_sat.jpg")
+    image = cv2.imread(os.path.join("images", image_num + "_mask.png"))
+    queue = []
+    heapq.heappush(queue, (0, origin))
+    g_score = {origin: 0}
+    f_score = {origin: distance(origin, target)}
+    directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]  # Up, Down, Left, Right
+    while queue:
+        current = heapq.heappop(queue)[1]
+        if current == target:
+            break
+        for direction in directions:
+            neighbor = (current[0] + direction[0], current[1] + direction[1])
+
+            if 0 <= neighbor[0] < image.shape[1] and 0 <= neighbor[1] < image.shape[0]:
+                if np.array_equal(image[neighbor[1], neighbor[0]], [255, 255, 255]):
+
+                    tentative_g_score = g_score[current] + 1
+                    if tentative_g_score < g_score.get(neighbor, float("inf")):
+                        g_score[neighbor] = tentative_g_score
+                        f_score[neighbor] = tentative_g_score + distance(
+                            neighbor, target
+                        )
+                        heapq.heappush(queue, (f_score[neighbor], neighbor))
+                        video_maker.change_pixel(neighbor[0], neighbor[1], (0, 255, 0))
     video_maker.release()
 
 
