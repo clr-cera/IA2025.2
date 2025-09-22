98ad0f89ed7ee522614ed8c0acf421c6ff50fc29
Gabriel Barbosa <50121055+GotemBarbosa@users.noreply.github.com>
2025-09-22 12:57:13 -0300

    fixes for video_examples

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import searches
 
-
 def main():
     searches.bfs((94, 182), (322, 630), "100712")
     searches.astar((94, 182), (322, 630), "100712")
     searches.dfs((94, 182), (322, 630), "100712")
+    searches.hill_climbing((94, 182), (322, 630), "100712")
     try:
-        searches.hill_climbing((94, 351), (581, 408), "100712")
+        # searches.hill_climbing((94, 351), (581, 408), "100712") exemplo conflito hill_climbing
+        searches.hill_climbing((94, 182), (322, 630), "100712")
+
     except Exception as error:
         print('couldnt find a path')
 
